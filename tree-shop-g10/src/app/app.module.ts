38dd1267a7c54089c20890e24a97602d23fed9ee
chairit1999 <chairit.2542@gmail.com>
2021-06-08 23:27:20 +0700

    add component addmin-oder oder addproduct header

--- tree-shop-g10/src/app/app.module.ts
+++ tree-shop-g10/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OderhistoryComponent } from './components/oderhistory/oderhistory.component';
+import { AddproductComponent } from './components/addproduct/addproduct.component';
+import { OderComponent } from './components/oder/oder.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddminOrderComponent } from './components/addmin-order/addmin-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OderhistoryComponent,
+    AddproductComponent,
+    OderComponent,
+    HeaderComponent,
+    AddminOrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ tree-shop-g10/src/app/components/addmin-order/addmin-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addmin-order',
+  templateUrl: './addmin-order.component.html',
+  styleUrls: ['./addmin-order.component.css']
+})
+export class AddminOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
